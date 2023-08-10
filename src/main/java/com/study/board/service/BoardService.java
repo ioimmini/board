c77f42d982f5737526bcb3ca501c1fdabe709084
ioimmini <jumin0114@gmail.com>
2023-08-10 20:12:16 +0900

    search

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,6 +3,8 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,13 +22,13 @@ public class BoardService {
 
         String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
 
-        UUID uuid = UUID.randomUUID();
+        UUID uuid = UUID.randomUUID(); //file 이름 ramdon
 
         String fileName = uuid + "_" + file.getOriginalFilename();
 
         File saveFile = new File(projectPath, fileName);
 
-        file.transferTo(saveFile);
+        file.transferTo(saveFile); //
 
         board.setFilename(fileName);
         board.setFilepath("/files/" + fileName);
@@ -35,9 +37,14 @@ public class BoardService {
 
     }
     //게시글 리스트 처리
-    public List<Board> boardList() {
+    public Page<Board> boardList(Pageable pageable) {
 
-        return boardRepository.findAll();
+        return boardRepository.findAll(pageable);
+    }
+
+    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable){
+
+        return boardRepository.findByTitleContaining(searchKeyword, pageable);
     }
 
     //특정 게시글 불러오기
